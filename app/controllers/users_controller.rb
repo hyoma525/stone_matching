9ed2d99337183c5bcdf319baf5bdcd11a605b77f
hyoma525 <j150055okubo@gmail.com>
2023-07-06 12:35:43 +0900

    マイページデザインの変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts
+    @power_stones = @user.have_stone_power_stones
   end
 
   def index
